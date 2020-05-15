37830cc5f5c676af05a69cbd41f78e82e7b28bc1
zhouyf <zhouyf@cemyun.com>
2020-05-15 14:08:50 +0800

    init

---
+++ CMYLog/CMYLog/CMYLog.h
@@ -0,0 +1,19 @@
+//
+//  CMYLog.h
+//  CMYLog
+//
+//  Created by zyF on 2020/5/15.
+//  Copyright © 2020 jojo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMYLog.
+FOUNDATION_EXPORT double CMYLogVersionNumber;
+
+//! Project version string for CMYLog.
+FOUNDATION_EXPORT const unsigned char CMYLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMYLog/PublicHeader.h>
+
+
