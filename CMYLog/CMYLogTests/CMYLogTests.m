0c5d9782ed8627f25b41e2eea165933785ca535d
Zhouyf14 <31618029+zhouyf14@users.noreply.github.com>
2020-05-15 17:55:52 +0800

    Update CMYLogTests.m
    move test

--- CMYLog/CMYLogTests/CMYLogTests.m
+++ CMYLog/CMYLogTests/CMYLogTests.m
@@ -8,7 +8,7 @@
 
 #import <XCTest/XCTest.h>
 
-@interface CMYLogTests : XCTestCase
+@interface CMYLogTests : NSObject
 
 @end
 
@@ -29,9 +29,9 @@
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
-    [self measureBlock:^{
+    //[self measureBlock:^{
         // Put the code you want to measure the time of here.
-    }];
+    //}];
 }
 
 @end
